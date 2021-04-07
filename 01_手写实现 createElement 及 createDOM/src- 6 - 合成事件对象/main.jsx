9b109b4eb9d6906de17f6cedb6adb6a490076556
宿愿Cc <375642570@qq.com>
2021-04-07 09:41:11 +0800

    feat(event): 新增 合成事件对象

---
+++ 01_手写实现 createElement 及 createDOM/src- 6 - 合成事件对象/main.jsx
@@ -0,0 +1,62 @@
+import React, { createElement } from "./react";
+import ReactDOM from "./react-dom";
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  /**
+   * div 点击事件
+   * @param {*} event
+   */
+  handleDivClick = (event) => {
+    console.log("div 发生点击(冒泡)");
+  };
+
+  /**
+   * 同步更新 - 当前已经是异步更新了，内部实现了合成事件
+   */
+  increment = (event) => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+
+    console.log("event:", event);
+
+    setTimeout(() => {
+      console.log("setTimeout event:", event);
+    });
+  };
+
+  /**
+   * 异步更新(自动动)
+   */
+  incrementAsync = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+
+    // this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div onClick={this.handleDivClick}>
+        {this.state.count}
+        <br />
+        <button onClick={this.increment}>同步更新+</button>
+        <br />
+        <button onClick={this.incrementAsync}>异步更新+</button>
+      </div>
+    );
+  }
+}
+
+const element = <Counter> </Counter>;
+
+ReactDOM.render(element, document.getElementById("root"));
